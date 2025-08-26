b501b20a6044b9b8100a1c6b506cdfe05c41fc94
ausstrict-sudo <ausstrict@gmail.com>
2025-08-26 19:09:00 +0100

    Add .next/static/chunks/src_app_pos_page_tsx_a993527f._.js 1756231701609

---
+++ .next/static/chunks/src_app_pos_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a129c662._.js",
+  "static/chunks/[root-of-the-server]__01bc6f32._.js",
+  "static/chunks/[next]_internal_font_google_inter_4617584a_module_790be3fb.css"
+],
+    source: "dynamic"
+});
